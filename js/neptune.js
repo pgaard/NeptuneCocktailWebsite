7210989692ace86fefb68cb2762afaf0d779a81c
Peter Gaard <pgaard@gmail.com>
2017-11-25 20:15:59 -0600

    videos and images

--- js/neptune.js
+++ js/neptune.js
@@ -3,7 +3,7 @@
  */
 var app = angular.module("neptuneApp", ["firebase"]);
 
-app.controller("NeptuneController", function($scope, $firebaseArray) {
+app.controller("NeptuneController", function($scope, $firebaseArray, $sce) {
     var ref = firebase.database().ref().child("gig");
     $scope.gigs = $firebaseArray(ref);
 
@@ -14,4 +14,44 @@ app.controller("NeptuneController", function($scope, $firebaseArray) {
     $scope.futureGig = function(gig){
         return new Date(gig.Date) >= $scope.tomorrow;
     }
+
+    $scope.videos =
+        [
+            {
+                title: "Dead or Alive",
+                subtitle: "Bon Jovi",
+                image: "wanted.png",
+                link: $sce.trustAsResourceUrl("https://www.youtube.com/embed/Ut0CEX_Sgdg")
+            },
+            {
+                title: "Sampler",
+                subtitle: "A bunch of songs",
+                image: "sampler.png",
+                link: $sce.trustAsResourceUrl("https://www.youtube.com/embed/yxfmBpjqknc")
+            },
+            {
+                title: "Rock of Ages",
+                subtitle: "Def Leppard",
+                image: "rockofages.png",
+                link: $sce.trustAsResourceUrl("https://www.youtube.com/embed/zjtklQuag9Q")
+            },
+            {
+                title: "Purple Rain",
+                subtitle: "Prince",
+                image: "purplerain.png",
+                link: $sce.trustAsResourceUrl("https://www.youtube.com/embed/IVQ0-y9DGJE")
+            },
+            {
+                title: "Sweet Child O Mine",
+                subtitle: "Guns n Roses",
+                image: "sweetchild.png",
+                link: $sce.trustAsResourceUrl("https://www.youtube.com/embed/Fd99gNDSBxE")
+            },
+            {
+                title: "Everlong",
+                subtitle: "Foo Fighters",
+                image: "everlong.png",
+                link: $sce.trustAsResourceUrl("https://www.youtube.com/embed/plDiKnmEzso")
+            }];
 });
+
